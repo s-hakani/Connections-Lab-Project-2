cad287fad0f96f1e6b8f6576d2644873ab8d8df3
Sarah Hakani <sarah.hakani@gmail.com>
2021-11-11 15:54:57 -0500

    another attempt at pushing back to github

--- public/sketch.js
+++ public/sketch.js
@@ -26,11 +26,12 @@ const TIME_BETWEEN_RANDOMIZATIONS = 1000; // milliseconds between new randoms
 
 function setup() {
     let myCanvas = createCanvas(window.innerWidth,window.innerHeight);
-  
+
     //Set the parent of the canvas to an exisitng html element's id value 
     myCanvas.parent("canvas-container");
     origC1 = color(255);
     origC2 = color(63, 191, 191);
+
     for(let y=0; y<height; y++){
       origN = map(y,0,height,0,1);
       origNewC = lerpColor(origC1,origC2,origN);
@@ -46,7 +47,7 @@ function draw() {
     colorMode(HSB, 100);
     // Home page dots moving sketch
     if (pageNumber == -1) {
-      // *** TO-DO: How to make the path that the circles follow fade as the sketch continues? 
+      // *** CHALLENGE TO-DO: How to make the path that the circles follow fade as the sketch continues? 
       for (let i = 0; i < 10; i++) {
         fill(frameCount % 255);
         ellipse(
@@ -58,42 +59,113 @@ function draw() {
       }
   
     if (pageNumber == 2) {
-      colorMode(RGB);
-  
-      let arrayLength = prayerArray.length;
-      let randomPrayer = (Math.floor(random(0,arrayLength)));
-      
-      // in the p5 code, background is in set up 
-      textSize(24);
-      noStroke();
-      fill(random(80),random(80),random(200), 70);
-      if (Date.now() - timeLastUpdated > TIME_BETWEEN_RANDOMIZATIONS) {
-        text(prayerArray[randomPrayer], random(window.innerWidth)-300, random(window.innerHeight));
-      timeLastUpdated = Date.now();
-      }
+
+      //This is the flower drawing
+      push();
+        //colorMode(HSB, 360, 100, 100); 
+        path.display();
+      pop();
+
+      push();
+      // This is the prayer gallery prayers drawing 
+        let arrayLength = prayerArray.length;
+        let randomPrayer = (Math.floor(random(0,arrayLength)));
+        // in the p5 code, background is in set up 
+        textSize(24);
+        noStroke();
+        colorMode(RGB);
+        fill(random(80),random(80),random(200), 70);
+        if (Date.now() - timeLastUpdated > TIME_BETWEEN_RANDOMIZATIONS) {
+          text(prayerArray[randomPrayer], random(window.innerWidth)-300, random(window.innerHeight));
+        timeLastUpdated = Date.now();
+        }
+      pop();
     }
   }
 
-  function mouseReleased() {
-    drawFlower(random(50))
-      //Grab mouse position
-    let mousePos = { x: mouseX, y: mouseY };
-    //Send mouse position object to the server
-    socket.emit('position', mousePos);
-    console.log(mousePos);
-   }
+//Helper functions to draw flowers 
 
-   function drawFlower(size) {
-     push();
+function mousePressed() {
+  if (pageNumber == 2) {
+    path = new Path();
+  }
+}
+
+function mouseDragged() {
+  if (pageNumber == 2) {
+    path.addPoint(mouseX, mouseY);
+  }
+}
+
+
+function mouseReleased() {
+  if (pageNumber == 2) {
+    drawFlower(random(50));
+  }
+}
+
+// I want the flower to be different each time 
+
+function drawFlower(size) {
+  if (pageNumber == 2) {
+    push(); 
       fill("yellow")
-      socket.on('coordinates', function(position) {
-        ellipse(position.x, position.y, 30);
-        translate (position.x,position.y);
-        fill(random(180,260), random(20,60), 100, .5);
-        for (let i = 0; i < 10; i ++) {
-          ellipse(0, 30, 20, size);
-          rotate(PI/5);
-        }
-    })  
+      ellipse(mouseX, mouseY, 30);
+      translate (mouseX,mouseY)
+      fill(random(180,260), random(20,60), 100, 0.5);
+      for (let i = 0; i < 10; i ++) {
+        ellipse(0, 30, 20, size);
+        rotate(PI/5);
+      }
     pop();
   }
+}
+
+//Creating the path that allows for calligraphy brush feeling 
+class Path {
+  constructor() {
+    this.pts = [];
+    this.size = 5; // size of brush
+    this.spacing = 3; // spacing between points
+    this.hue = 90; // start value
+    this.hues = [] // keep track of the rgb for each point
+  }
+
+  get lastPt() {
+    return this.pts[this.pts.length - 1];
+  }
+
+  addPoint(x, y) {
+    if (this.pts.length < 1) {
+      this.pts.push(new p5.Vector(x, y));
+      this.hues.push(this.hue)
+      return;
+    }
+
+    const nextPt = new p5.Vector(x, y);
+    let d = p5.Vector.dist(nextPt, this.lastPt);
+
+    while (d > this.spacing) {
+      const diff = p5.Vector.sub(nextPt, this.lastPt);
+      diff.normalize();
+      diff.mult(this.spacing)
+      this.pts.push(p5.Vector.add(this.lastPt, diff));
+      d -= this.spacing;
+      // updated code to make the gradient move slower from starting point to ending point 
+      this.hue = (this.hue + 0.5) % 360; // for each new point, update the hue
+      this.hues.push(this.hue)
+    }
+  }
+  
+  
+  display() {
+    //path = new Path(); 
+    noStroke();
+    for (let i = 0; i < this.pts.length; i++) {
+      const p = this.pts[i];
+      fill(this.hues[i], 100, 80, .8)
+      // wanted it to feel like a calligraphy brush (ellipse not circle)
+      ellipse(p.x, p.y+30, this.size, 15);
+    }
+  }
+}
